129a2d7b934b94dafd0f53b741a84137cdcd76eb
Denis Suvolokin <budden@list.ru>
2018-02-21 11:51:41 +0400

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
  class Hero {
   id: number;
   name:string;
+  bio:string;
 }
 
 @Component({
@@ -15,7 +16,26 @@ export class AppComponent {
   public title = 'tour of heroes';
   public hero:Hero = {
     id: 1,
-    name: 'Windstorm'
-  }
-
+    name: 'Windstorm',
+    bio: 'wind and thunder controller'
+  };
+  public selectedHero:Hero;
+  public heroes = HEROES;
+  onSelect(f:Hero){
+    this.selectedHero = f;
+    console.log(this.selectedHero)
+  };
 }
+
+let HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice", "bio": "very pretty guy" },
+  { "id": 12, "name": "Narco", "bio": "dangerous and retard" },
+  { "id": 13, "name": "Bombasto", "bio": "bomb lover" },
+  { "id": 14, "name": "Celeritas", "bio": "marakasses master" },
+  { "id": 15, "name": "Magneta", "bio": "electrifier and Dynama's brother" },
+  { "id": 16, "name": "RubberMan", "bio": "flexible like bubble gum" },
+  { "id": 17, "name": "Dynama", "bio": "lightning in flesh and Magneta's brother" },
+  { "id": 18, "name": "Dr IQ", "bio": "all siences doctor" },
+  { "id": 19, "name": "Magma", "bio": "hot chick" },
+  { "id": 20, "name": "Tornado", "bio": "big and furious" }
+];
